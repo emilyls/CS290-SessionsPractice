debb4d3cddb12d1878176176672d2f3a848339ff
Ubuntu <ubuntu@ip-172-31-39-58.us-west-2.compute.internal>
2015-11-21 05:33:07 +0000

    can't get api to work on node

--- list.js
+++ list.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var http = require('http');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var apiKey ='fa7d80c48643dfadde2cced1b1be6ca1';
 
 app.use(bodyParser.urlencoded({ extended: false} ));
 app.use(session({secret:'Emily'}));
@@ -40,9 +42,28 @@ app.post('/', function(req, res) {
   }
 
   if(req.body['Add Item']) {
-    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
-    req.session.curId++;
-  }
+    req.session.toDo.push({"name":req.body.name, "city":req.body.city, "temperature":req.body.weather, "id":req.session.curId});
+    req.session.curId++; 
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    var options = {
+      host: 'http://api.openweathermap.org',
+      path: '/data/2.5/weather?q=' + city + '&appid=' + apiKey
+    };
+    callback = function(response) {
+      var str = '';
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+      response.on('end', function() {
+        console.log(str);
+      });
+    }
+    http.request(options,callback).end();
+    var data = JSON.parse(response.Content);
+    if (data.main.temp >= ((req.body.weather + 459.67) * (5/9))) {
+      city.style.color = green;
+    }
+ }
 
   if(req.body['Done']) {
     req.session.toDo = req.session.toDo.filter(function(e) {
